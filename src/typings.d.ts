28dc06ceeccdf92cc85afdcbe0098062a8bf634c
haw-Sauce <1242880434@qq.om>
2025-05-28 16:32:16 +0800

    refactor: eslint --fix

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,7 @@
-//声明.vue文件
-declare module "*.vue" {
-  import { DefineComponent } from "vue";
-  const component: DefineComponent<object, object, any>;
-  export default component;
+// 声明.vue文件
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent<object, object, any>
+  export default component
 }
