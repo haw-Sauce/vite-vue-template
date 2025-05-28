27e9da7f11fc5cd8c72b7d868c5c64fa50d0e1b5
haw-Sauce <1242880434@qq.om>
2025-05-28 21:33:55 +0800

    feat: 添加自动导入和全局组件类型定义文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: 'src/types/auto-imports.d.ts', // 自动生成的类型声明文件路径
+      imports: ['vue', 'vue-router', 'pinia'], // 自动导入的模块
+      eslintrc: { enabled: true },
+      vueTemplate: true, // default false
+      resolvers: [ElementPlusResolver()], // 使用 Element Plus 的解析器
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  // 设置别名
   resolve: {
     alias: {
       '@': path.resolve('./src'), // @代替src
